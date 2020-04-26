7c649d1bb0cc657f6db20c164a1e652a16dc533f
Prince Sumberia <broughtmeup@gmail.com>
2020-04-26 15:47:35 +0530

    Updates

---
+++ precache-manifest.ae8a73af9f7c634a27f536e49a23b6ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3a6f2133adefab4d80ac1e33b8524f9",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "6c7a8b1a9380997113ae",
+    "url": "/covid-19-tracker/static/css/main.7b78cfee.chunk.css"
+  },
+  {
+    "revision": "1cf7db583a8f4a9343f3",
+    "url": "/covid-19-tracker/static/js/2.5dcfc110.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/covid-19-tracker/static/js/2.5dcfc110.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c7a8b1a9380997113ae",
+    "url": "/covid-19-tracker/static/js/main.ca6ec9eb.chunk.js"
+  },
+  {
+    "revision": "3b4e6bf1dc0253137a26",
+    "url": "/covid-19-tracker/static/js/runtime-main.e40f93c2.js"
+  }
+]);
