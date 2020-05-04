99a12148f6af99498dce7e6798ae758ed7d9ddb8
Prince Sumberia <broughtmeup@gmail.com>
2020-05-04 16:18:33 +0530

    Add sizes file for media query

---
+++ src/styles/sizes.js
@@ -0,0 +1,13 @@
+export default {
+  up() {},
+  down(size) {
+    const sizes = {
+      xs: "575.98px",
+      sm: "767.98px",
+      md: "991.98px",
+      lg: "1199.98px",
+      xl: "1350px",
+    };
+    return `@media (max-width: ${sizes[size]} )`;
+  },
+};
