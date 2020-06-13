944d267f2a97f169ef89445430d07621556b119b
Prince Sumberia <broughtmeup@gmail.com>
2020-06-13 15:22:52 +0530

    Organized imports

--- src/components/ResponsiveTable.js
+++ src/components/ResponsiveTable.js
@@ -1,4 +1,4 @@
-import React from "react";
+import Paper from "@material-ui/core/Paper";
 import { makeStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
@@ -6,7 +6,7 @@ import TableCell from "@material-ui/core/TableCell";
 import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
-import Paper from "@material-ui/core/Paper";
+import React from "react";
 
 const useStyles = makeStyles({
   table: {
